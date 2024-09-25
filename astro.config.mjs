54740acc1ee5aac70204c3fabe7d824d0ff2c33e
LeoOoOo <castillofernandezleonardo778@gmail.com>
2024-09-25 16:19:26 -0400

    Dev Books Store v1.1.0 fixed: UI added: footer component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,20 @@
 import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind'
 
+
 import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
+    output: 'hybrid',
     integrations: [tailwind()],
-
-    //   adapter: vercel()
-    env: {
-        schema: {
-            SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
-            SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' })
+    experimental: {
+        env: {
+            schema: {
+                SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
+                SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public' })
+            }
         }
     },
 
